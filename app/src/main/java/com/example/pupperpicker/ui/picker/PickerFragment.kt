1787e8759a0982cc774281fc94903a098703ab54
Roarw <Zalwio@hotmail.dk>
2023-02-18 16:53:07 -0800

    Woof woof

--- app/src/main/java/com/example/pupperpicker/ui/picker/PickerFragment.kt
+++ app/src/main/java/com/example/pupperpicker/ui/picker/PickerFragment.kt
@@ -1,18 +1,22 @@
 package com.example.pupperpicker.ui.picker
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.example.pupperpicker.R
 import com.example.pupperpicker.databinding.FragmentPickerBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.random.Random
 
 @AndroidEntryPoint
 class PickerFragment : Fragment() {
 
     private val pickerViewModel: PickerViewModel by activityViewModels()
+    private var dogSoundIDs: List<Int>? = null
 
     private var _binding: FragmentPickerBinding? = null
 
@@ -35,9 +39,33 @@ class PickerFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         pickerViewModel.dogResponse.observe(viewLifecycleOwner) { dogResponse ->
-            binding.textPicker.text = dogResponse.message
+            binding.removeThisText.text = dogResponse.message
         }
         pickerViewModel.getDog() // Force dog response to change
+
+        setupSounds()
+
+        binding.pupperButton.setOnClickListener {
+            playDogSound()
+        }
+    }
+
+    private fun setupSounds(){
+        dogSoundIDs = listOf<Int>(
+            R.raw.bark1,
+            R.raw.bark2,
+            R.raw.bark3,
+            R.raw.bark4,
+            R.raw.bark5,
+            R.raw.bark6
+        )
+    }
+
+    private fun playDogSound(){
+        if (dogSoundIDs != null) {
+            val randomIndex = Random.nextInt(dogSoundIDs!!.size)
+            MediaPlayer.create(context, dogSoundIDs!![randomIndex]).start()
+        }
     }
 
     override fun onDestroyView() {
