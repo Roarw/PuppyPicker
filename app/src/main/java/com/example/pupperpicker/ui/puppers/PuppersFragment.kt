37e0451140e319ac436bb8d9ea83091dc7175eaf
Roarw <Zalwio@hotmail.dk>
2023-02-20 16:19:02 -0800

    Pupper view can not fetch favorite dogs

--- app/src/main/java/com/example/pupperpicker/ui/puppers/PuppersFragment.kt
+++ app/src/main/java/com/example/pupperpicker/ui/puppers/PuppersFragment.kt
@@ -1,12 +1,13 @@
 package com.example.pupperpicker.ui.puppers
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.activityViewModels
+import com.example.pupperpicker.PuppyPickerApplication
 import com.example.pupperpicker.databinding.FragmentPuppersBinding
 
 class PuppersFragment : Fragment() {
@@ -16,22 +17,29 @@ class PuppersFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private val puppersViewModel: PuppersViewModel by activityViewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val puppersViewModel =
-            ViewModelProvider(this).get(PuppersViewModel::class.java)
-
         _binding = FragmentPuppersBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textPuppers
-        puppersViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        puppersViewModel.dogURLs.observe(viewLifecycleOwner) { dogURLs ->
+            if (dogURLs != null)
+            {
+                Log.d("DataFun", dogURLs.toString())
+            }
+        }
+
+        puppersViewModel.loadFavoriteDogs((requireActivity().applicationContext as PuppyPickerApplication).dbHelper)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/pupperpicker/ui/puppers/PuppersViewModel.kt
+++ app/src/main/java/com/example/pupperpicker/ui/puppers/PuppersViewModel.kt
@@ -1,13 +1,30 @@
 package com.example.pupperpicker.ui.puppers
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pupperpicker.data.DBHelper
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class PuppersViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is puppers Fragment"
+    var dogURLs: MutableLiveData<List<String>?> = MutableLiveData()
+    var loading: MutableLiveData<Boolean> = MutableLiveData()
+
+    fun loadFavoriteDogs(dbHelper: DBHelper?) {
+        if (dbHelper != null) {
+            viewModelScope.launch {
+                withContext(Dispatchers.IO) {
+                    loading.postValue(true)
+
+                    // Load favorite dogs
+                    dogURLs.postValue(dbHelper.getAllURLs())
+
+                    loading.postValue(false)
+                }
+            }
+        }
     }
-    val text: LiveData<String> = _text
 }
